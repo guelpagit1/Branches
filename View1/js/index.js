56715f8a19be834dc593f1db6cbbd8eedb5eda1c
Maximiliano <mguelpa@gmail.com>
2019-10-27 20:30:50 -0300

    A message

---
+++ View1/js/index.js
@@ -0,0 +1,16 @@
+window.addEventListener("load", function() {
+
+    var btn = document.getElementById("greet-btn");
+    
+    btn.addEventListener('click', ejecutar);  
+
+});
+
+
+
+
+function ejecutar() {
+
+    alert("Hello World!");
+
+}
